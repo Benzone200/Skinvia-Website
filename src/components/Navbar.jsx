c0dc82ffd0d1515cfdfa826b45d3a9aa4a55b674
Benzone200 <dunuchisom1@gmail.com>
2025-11-04 00:26:26 +0100

    Design changes and bug fixes for certain screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "../assets/images/logo.png";
 import search from "../assets/images/search.png";
 import cart from "../assets/images/cart.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -28,16 +29,21 @@ const Navbar = () => {
         {/* Desktop & Tablet Menu */}
         <ul className="hidden md:flex md:flex-row md:items-center md:gap-10 lg:gap-20">
           <li className="text-[#BF6159] font-medium text-[18px] hover:text-[#a54c46] transition">
-            <a href="#">Home</a>
+            <Link to ="/"> Home</Link>
+            
           </li>
           <li className="text-[#BF6159] font-medium text-[18px] hover:text-[#a54c46] transition">
-            <a href="#">Products</a>
+            <Link to ="/products"> Products</Link>
+         
           </li>
           <li className="text-[#BF6159] font-medium text-[18px] hover:text-[#a54c46] transition">
-            <a href="#">About</a>
+           <Link to ="/about">About </Link>
+          
           </li>
-          <img src={search} alt="Search" className="w-[18px] h-[18px]" />
-          <img src={cart} alt="Cart" className="w-[20px] h-[20px]" />
+          <img src={search} alt="Search" className="w-[18px] h-[18px] cursor-pointer" />
+          <Link to ="/cart">
+          <img src={cart} alt="Cart" className="w-[20px] h-[20px] cursor-pointer" />
+          </Link>
         </ul>
       </nav>
 
@@ -46,18 +52,22 @@ const Navbar = () => {
         <div className="md:hidden bg-[#D9D9D9] px-6 py-4 space-y-4">
           <ul className="flex flex-col gap-3">
             <li className="text-[#BF6159] font-medium text-[18px]">
-              <a href="#">Home</a>
+              <Link to ="/"> Home</Link>
+         
             </li>
             <li className="text-[#BF6159] font-medium text-[18px]">
-              <a href="#">Products</a>
+              <Link to ="/products"> Products</Link>
+             
             </li>
             <li className="text-[#BF6159] font-medium text-[18px]">
-              <a href="#">About</a>
+              <Link to ="/about"> About</Link>
             </li>
           </ul>
-          <div className="flex gap-5 pt-2">
-            <img src={search} alt="Search" className="w-[18px] h-[18px] " />
-            <img src={cart} alt="Cart" className="w-[20px] h-[20px]" />
+          <div className="flex gap-3 pt-2">
+            <img src={search} alt="Search" className="w-[18px] h-[18px] cursor-pointer " />
+            <Link to ="/cart">
+            <img src={cart} alt="Cart" className="w-[20px] h-[20px] cursor-pointer" />
+            </Link>
           </div>
         </div>
       )}
